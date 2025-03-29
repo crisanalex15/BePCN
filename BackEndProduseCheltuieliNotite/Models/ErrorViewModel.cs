1ca500eb56ca532776f2706b1ff6f40062820cfe
crisanalex15 <alex.crisan@student.upt.ro>
2025-03-30 00:30:27 +0200

    Proiect prototip START

---
+++ BackEndProduseCheltuieliNotite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BackEndProduseCheltuieliNotite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
